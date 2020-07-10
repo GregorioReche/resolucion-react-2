a854110c3078357f8fb5849c5571a870d77602cc
Gregorio Reche <grego@MacBook-Pro-de-Gregorio.local>
2020-07-10 18:54:18 -0300

    Creacion de componente unico para mostrar metricas llamado SmallCard. Se implementa en ContentRowTop.js

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import ProductsInDB from './ProductsInDB';
-import AmountOfProductsInDB from './AmountOfProductsInDB';
-import AmountOfUsersInDB from './AmountOfUsersInDB';
+import SmallCard from './SmallCard';
 
 function ContentRowTop(){
     return (
         <div className="row">
         {/* <!-- Products in DB --> */}
-            <ProductsInDB />
+            <SmallCard 
+                title='Products in Data Base'
+                color='primary' 
+                cuantity='135' 
+                icon='fas fa-clipboard-list fa-2x text-gray-300'
+            />
+          
 
         {/* <!-- $$$ of all products in DB --> */}
-            <AmountOfProductsInDB />
+            <SmallCard 
+                title='Amounts in products' 
+                color='success'
+                cuantity='$546.456' 
+                icon='fas fa-dollar-sign fa-2x text-gray-300'
+            />
+           
 
         {/* <!-- Amount of users in DB --> */}
-            <AmountOfUsersInDB />
+            <SmallCard 
+                title='Users quantity' 
+                color='warning'
+                cuantity='38' 
+                icon='fas fa-user-check fa-2x text-gray-300'
+            />
+            
+
     </div>
     )
 }
