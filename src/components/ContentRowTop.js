e64393ee82cdbbfa1ee3bf8ffb9bac0e0ed330c4
Gregorio Reche <grego@MacBook-Pro-de-Gregorio.local>
2020-07-11 00:49:02 -0300

    Se crea un array con los datos de los las tarjetas pequeñas y se pasan en un array al componente las renderiza

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import SmallCard from './SmallCard';
 
+/*  Cada set de datos es un objeto literal */
+
+/* <!-- Products in DB --> */
+
+let productsInDB = {
+    title: 'Products in Data Base',
+    color: 'primary', 
+    cuantity: '135',
+    icon: 'fas fa-clipboard-list fa-2x text-gray-300'
+}
+
+/* <!-- $$$ of all products in DB --> */
+
+let amountOfProducts = {
+    title:'Amounts in products', 
+    color:'success',
+    cuantity:'$546.456', 
+    icon:'fas fa-dollar-sign fa-2x text-gray-300'
+}
+
+/* <!-- Amount of users in DB --> */
+
+let amountOfUsers = {
+    title:'Users quantity' ,
+    color:'warning',
+    cuantity:'38',
+    icon:'fas fa-user-check fa-2x text-gray-300'
+}
+
+let cartProps = [productsInDB, amountOfProducts, amountOfUsers];
+
 function ContentRowTop(){
     return (
         <div className="row">
-        {/* <!-- Products in DB --> */}
-            <SmallCard 
-                title='Products in Data Base'
-                color='primary' 
-                cuantity='135' 
-                icon='fas fa-clipboard-list fa-2x text-gray-300'
-            />
-          
 
-        {/* <!-- $$$ of all products in DB --> */}
-            <SmallCard 
-                title='Amounts in products' 
-                color='success'
-                cuantity='$546.456' 
-                icon='fas fa-dollar-sign fa-2x text-gray-300'
-            />
-           
+            <SmallCard atributes={cartProps} />
 
-        {/* <!-- Amount of users in DB --> */}
-            <SmallCard 
-                title='Users quantity' 
-                color='warning'
-                cuantity='38' 
-                icon='fas fa-user-check fa-2x text-gray-300'
-            />
-            
-
-    </div>
+        </div>
     )
 }
 
--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -2,21 +2,25 @@ import React from 'react';
 
 function SmallCard(props){
     return(
-        <div className="col-md-4 mb-4">
-                <div className={`card border-left-${props.color} shadow h-100 py-2`}>
+        props.atributes.map( (atributes) => {
+            return (
+            <div className="col-md-4 mb-4">
+                <div className={`card border-left-${atributes.color} shadow h-100 py-2`}>
                     <div className="card-body">
                         <div className="row no-gutters align-items-center">
                             <div className="col mr-2">
-                                <div className={`text-xs font-weight-bold text-${props.color} text-uppercase mb-1`}> {props.title}</div>
-                                <div className="h5 mb-0 font-weight-bold text-gray-800">{props.cuantity}</div>
+                                <div className={`text-xs font-weight-bold text-${atributes.color} text-uppercase mb-1`}> {atributes.title}</div>
+                                <div className="h5 mb-0 font-weight-bold text-gray-800">{atributes.cuantity}</div>
                             </div>
                             <div className="col-auto">
-                                <i className={props.icon}></i>
+                                <i className={atributes.icon}></i>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            )}
+        )
     )
 }
 
